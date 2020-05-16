765207f45f952e1518ddb991ce0c73402385c8e6
960201 <1300541222@qq.com>
2020-05-16 19:17:34 +0800

    二次修改

--- warehouse.sh
+++ warehouse.sh
@@ -8,6 +8,6 @@ git add .
 # 提交到本地仓库
 git commit -m '二次修改'
 # 连接远程仓库地址
-git remote add origin https://github.com/960201/vuepress-blog.git
+# git remote add origin https://github.com/960201/vuepress-blog.git
 # push到github仓库
-git push origin master 
+git push
